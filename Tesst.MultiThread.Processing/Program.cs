022f7ab7fe5e08e9a7a43c836a744312374a6995
John <john72831@hotmail.com>
2018-07-13 13:40:35 +0800

    加入專案檔案。

---
+++ Tesst.MultiThread.Processing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tesst.MultiThread.Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
